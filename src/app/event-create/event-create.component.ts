66523c6c715b5e607a27a812b7a58ff5b44af0f1
Edo Scalafiotti <scalae@amazon.com>
2020-06-24 15:40:15 +0100

    create event modal

--- src/app/event-create/event-create.component.ts
+++ src/app/event-create/event-create.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventCreateComponent implements OnInit {
 
+  colours: any = [
+    { label: 'Pink', value: '#E91E63' },
+    { label: 'Purple', value: '#9C27B0' },
+    { label: 'Blue', value: '#2196F3' },
+
+    { label: 'Green', value: '#4CAF50' },
+    { label: 'Yellow', value: '#FFC107' },
+
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
